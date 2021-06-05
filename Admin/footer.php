408835d7634f1c2658dfb7f95f0ad0811d6f0019
ankit <ac2000choudhary@gmail.com>
2021-06-05 17:04:19 +0530

    footer added

---
+++ Admin/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Event Planning Services</strong>
+    </p>
+    <p>
+    All Event Vendors | Catering Services | Wedding Planning | Event Planning | Birthday Planning
+    </p>
+  </div>
+</footer>
