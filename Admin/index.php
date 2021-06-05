8c26726c6a8e64719259d7124d76add476ff46b2
ankit <ac2000choudhary@gmail.com>
2021-06-05 17:04:41 +0530

    Admin Palane

---
+++ Admin/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>venueHut</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+</head>
+<body>
+<!-- Navbar section -->
+<div class="container-fluid">
+    <?php
+        include('navbar.php');
+    ?>
+</div>
+<!-- End navbar section -->
+
+
+
+<!-- footer section -->
+<div class="container-fluid">
+    <?php
+        include('footer.php');
+    ?>
+</div>
+<!-- End footer section -->
+</body>
+</html>
