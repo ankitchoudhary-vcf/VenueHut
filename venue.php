c663367f727ff5d6ca7a49c215a6f97afcf68269
ankit <ac2000choudhary@gmail.com>
2021-06-24 20:38:59 +0530

    venue updated

--- venue.php
+++ venue.php
@@ -1,6 +1,5 @@
 <?php
-
-include('conn.php');
+include("conn.php");
 
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
@@ -19,108 +18,143 @@ if (isset($_GET['id'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>venueHut</title>
-    <link rel="shortcut icon" type="image/jpg" href="./assets/image/favicon.png" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/jpg" href="./img/favicon.png" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Source+Code+Pro:700,900&display=swap" rel="stylesheet">
+
+    <!-- CSS Libraries -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="lib/slick/slick.css" rel="stylesheet">
+    <link href="lib/slick/slick-theme.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
 </head>
 
 <body>
-    <div class="wrapper">
-        <!-- Navbar section -->
-        <?php
-        include('navbar.php');
-        ?>
-        <!-- End navbar section -->
 
 
-        <!-- main section -->
-        <div class="main_content">
+    <!-- Nav Bar Start -->
+    <div class="nav">
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+                <a href="#" class="navbar-brand">MENU</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-
-            <div class=" container mx-4 mt-4">
-                <div class="title">
-                    <nav class="breadcrumb" aria-label="breadcrumbs">
-                        <ul>
-                            <li><a href="./">VenueHut</a></li>
-                            <li class="is-active"><a aria-current="page"><?php echo $service; ?></a></li>
-                        </ul>
-                    </nav>
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav mr-auto">
+                        <a href="./index.php" class="nav-item nav-link">Home</a>
+                        <a href="./index.php#services" class="nav-item nav-link">Services</a>
+                        <a href="./contact.php" class="nav-item nav-link">Contact Us</a>
+                    </div>
                 </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Nav Bar End -->
 
-                <div class="columns is-multiline m-4">
+    <!-- Bottom Bar Start -->
+    <div class="bottom-bar">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-md-3">
+                    <div class="logo">
+                        <a href="index.php">
+                            <img src="img/favicon.png" alt="Logo"> <span class="mx-2" style="font-weight: bold; font-size:large;">venueHut</span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <form action="./search.php" method="post">
+                        <div class="search">
+                            <input type="text" placeholder="Search" name="search">
+                            <button type="submit"><i class="fa fa-search"></i></button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Bottom Bar End -->
 
-                    <?php
+    <!-- Breadcrumb Start -->
+    <div class="breadcrumb-wrap">
+        <div class="container-fluid">
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item"><a href="./index.php">Home</a></li>
+                <li class="breadcrumb-item"><a href="./index.php#services">Services</a></li>
+                <li class="breadcrumb-item active"><?php echo $service; ?></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Breadcrumb End -->
 
-                    $ssq = "SELECT * FROM `venues` WHERE `service_Id` = $id";
+    <!-- Product List Start -->
+    <div class="product-view">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <?php
 
-                    $result = mysqli_query($conn, $ssq);
+                        $ssq = "SELECT * FROM `venues` WHERE `service_Id` = $id";
 
-                    while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-                    ?>
+                        $result = mysqli_query($conn, $ssq);
 
-                        <!-- Services Section -->
-                        <div class="column is-full is-desktop">
-                            <div class="card">
-                                <div class="card-content">
-                                <div class="columns">
-                                        <div class="column is-one-third is-desktop">
-                                            <figure class="image is-256x256 is-square">
-                                                <img class="has-ratio" src="../upload/<?php echo trim($row['venue_image']); ?>">
-                                            </figure>
-                                        </div>
-                                        <div class="column">
-                                            <div class="content">
-                                            <div class="content">
-                                                <p>
-                                                    <strong><?php echo $row['venue_name']; ?></strong>
-                                                    <br>
-                                                    <?php echo $row['venue_description']; ?>
-                                                    <br>
-                                                    <br>
-                                                    <small><i class="fas fa-map-marker-alt"></i> <?php echo $row['venue_location']; ?></small>
-                                                    <br>
-                                                    <strong><a href="contact.php" class="button is-primary mt-6">Rs. <?php echo $row['venue_price']; ?> /-</a></strong>
-                                                </p>
-                                            </div>
-                                            </div>
-                                        </div>
+                        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                        ?>
+                            <div class="col-md-4">
+                                <div class="product-item">
+                                    <div class="product-title">
+                                        <a href="#"><?php echo $row['venue_name']; ?></a>
+                                    </div>
+                                    <div class="product-image">
+                                        <a href="./venue-detail.php?id=<?php echo $row['venue_id']; ?>">
+                                            <img src="./upload/<?php echo trim($row['venue_image']); ?>" alt="Product Image" height="350px" width="auto">
+                                        </a>
+                                    </div>
+                                    <div class="product-price">
+                                        <h3 style="font-size:medium;"><i class="fas fa-map-marker-alt"></i> <?php echo $row['venue_location']; ?></h3>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <!-- End Services Section -->
-
-
-                    <?php
-                    }
-
-                    ?>
-
-
+                        <?php
+                        }
+                        ?>
+                    </div>
                 </div>
-
             </div>
-
-            <!-- footer section -->
-            <div>
-                <?php
-                include('footer.php');
-                ?>
-            </div>
-            <!-- End footer section -->
-
         </div>
-        <!-- End main section -->
-
     </div>
+    <!-- Product List End -->
+
+    <!-- Footer Start -->
+
+    <?php
+    include('./footer.php');
+    ?>
+    <!-- Footer End -->
+
+    <!-- Back to Top -->
+    <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/slick/slick.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
-<script src="./assets/js/script.js"></script>
-<script src="./assets/js/message.js"></script>
-<script src="./assets/js/modal.js"></script>
 
 </html>
