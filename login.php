02a7fe783fd1780e694bcb325a44b810fd3f435b
ankit <ac2000choudhary@gmail.com>
2021-07-02 19:14:10 +0530

    login verfication added

--- login.php
+++ login.php
@@ -4,8 +4,7 @@ include('conn.php');
 
 session_start();
 
-if(isset($_SESSION['user']))
-{
+if (isset($_SESSION['user'])) {
     header('Location: ./');
 }
 
@@ -19,9 +18,14 @@ if (isset($_POST['login'])) {
     $result = mysqli_query($conn, $sq);
     $ctr = 0;
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-        if (password_verify($password, $row['user_password'])) {
-            $ctr = 1;
-            $user_id  = $row['user_id'];
+        if ($row['status'] == 'ENABLE') {
+            if (password_verify($password, $row['user_password'])) {
+                $ctr = 1;
+                $user_id  = $row['user_id'];
+            }
+        }
+        else{
+            $ctr = -1;
         }
     }
     // echo $user_id;
@@ -29,13 +33,21 @@ if (isset($_POST['login'])) {
     if ($ctr == 1) {
         $_SESSION['user'] = $user_id;
         $_SESSION['success'] = "Successfully Logged In";
-        
+
         if (isset($_SERVER["HTTP_REFERER"])) {
             header("Location: " . $_SERVER["HTTP_REFERER"]);
         }
-        
-    } else {
+    } else if ($ctr == -1) {
 ?>
+        <article class="message is-warning">
+            <div class="message-header">
+                <p>Please first Verify your email address.</p>
+                <button class="delete" aria-label="delete"></button>
+            </div>
+        </article>
+    <?php
+    } else {
+    ?>
         <article class="message is-warning">
             <div class="message-header">
                 <p>Invalid Username or Password</p>
@@ -57,12 +69,27 @@ if (isset($_POST['login'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VenueHut</title>
-    <link rel="shortcut icon" type="image/jpg" href="./img/favicon.png"/>
+    <link rel="shortcut icon" type="image/jpg" href="./img/favicon.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.2/css/bulma.min.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 </head>
 
 <body>
+
+    <?php
+    if (isset($_SESSION['success_message'])) {
+    ?>
+        <article class="message is-success">
+            <div class="message-header">
+                <p><?php echo $_SESSION['success_message']; ?></p>
+                <button class="delete" aria-label="delete"></button>
+            </div>
+        </article>
+    <?php
+    unset($_SESSION['success_message']);
+    }
+    ?>
+
     <div class="container is-light" style="margin-top: 100px;">
         <div id="login" class="my-6 px-6 notification is-white container column is-mobile is-one-third-desktop is-center">
             <div class="content">
